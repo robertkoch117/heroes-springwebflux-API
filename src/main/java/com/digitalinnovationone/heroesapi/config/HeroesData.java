7b0883747474399f0d692be930e6999194858f0d
robertkoch117 <robertkoch117@gmail.com>
2021-04-29 15:32:27 -0300

    Commit API Herois Java

---
+++ src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
@@ -0,0 +1,44 @@
+package com.digitalinnovationone.heroesapi.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import static com.digitalinnovationone.heroesapi.constans.HeroesConstant.REGION_DYNAMO;
+import static com.digitalinnovationone.heroesapi.constans.HeroesConstant.ENDPOINT_DYNAMO;
+
+
+public class HeroesData {
+    public static void main(String [] args) throws Exception{
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Heroes_Table");
+        Item hero = new Item()
+                .withPrimaryKey("id", "1")
+                .withString("name", "Mulher Maravilha")
+                .withString("universe", "DC Comics")
+                .withNumber("films", 3);
+
+        PutItemOutcome outcome = table.putItem(hero);
+    }
+}
